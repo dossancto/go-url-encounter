5086ed8b2caf54ea77f52f6ac038d3fd2fde4d36
lu-css <dossantossilvalucas963@gmail.com>
2023-01-15 01:20:25 -0300

    Dependencies

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/lu-css/go-url-encounter/pkg/database"
 	"github.com/lu-css/go-url-encounter/pkg/server"
 )
 
 func main() {
+  database.Connect()
   server.Start()
 }
